5e24a0576e174949a79b3d5f3c0daae8bd0e85ea
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-03-03 03:31:55 +0300

    Added text markups.

---
+++ markups/text/buttons.py
@@ -0,0 +1,7 @@
+from aiogram.types import KeyboardButton
+
+back_button = KeyboardButton('Назад')
+go_button = KeyboardButton('Начнём')
+no_button = KeyboardButton('Нет')
+yes_button = KeyboardButton('Да')
+cancel_button = KeyboardButton('Отмена')
---
+++ markups/text/keyboards.py
@@ -0,0 +1,29 @@
+from aiogram.types import ReplyKeyboardMarkup
+
+from .buttons import (
+    back_button,
+    no_button,
+    go_button,
+    yes_button,
+    cancel_button
+)
+
+back_keyboard = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    row_width=1
+).add(back_button)
+
+welcome_keyboard = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    row_width=2
+).add(go_button, no_button)
+
+restart_keyboard = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    row_width=2
+).add(yes_button, no_button)
+
+cancel_keyboard = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    row_width=1
+).add(cancel_button)
