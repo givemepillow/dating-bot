95aa0e728fb64126832a7f93b80ae97c6f180c4b
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-03-06 04:10:25 +0300

    Bot model refactoring.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
-from .tools import WebhookModel, PollingModel
-from .singletone import SingletonABC, Singleton
+from .PollingBotModel import PollingModel
+from .WebhookBotModel import WebhookModel
+from .Singletone import SingletonABC, Singleton
