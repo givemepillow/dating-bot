2b518ecb198ec85992185aa6d439db25afdf2370
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-03-08 16:13:54 +0300

    Unique control.

--- toolkit/MessageBox.py
+++ toolkit/MessageBox.py
@@ -7,18 +7,21 @@ from loader import bot
 
 
 class MessageBox:
-    _storage: {int: []} = dict()
+    _storage: {int: [Message]} = dict()
+    _messages: {int: {int}} = dict()
 
     @classmethod
-    def save(cls, message: Message, user_id: int):
-        if user_id not in cls._storage:
-            cls._storage[user_id] = []
-            cls._storage[user_id].append(message)
-        else:
+    def put(cls, message: Message, user_id: int):
+        if user_id not in cls._messages:
+            cls._messages[user_id] = set()
+        if message.message_id not in cls._messages[user_id]:
+            cls._messages[user_id].add(message.message_id)
+            if user_id not in cls._storage:
+                cls._storage[user_id] = []
             cls._storage[user_id].append(message)
 
     @classmethod
-    def get(cls, user_id: int) -> Message:
+    def get(cls, user_id: int) -> Message | None:
         if user_id in cls._storage and cls._storage[user_id]:
             return cls._storage[user_id].pop()
         else:
@@ -29,4 +32,6 @@ class MessageBox:
         with suppress(MessageCantBeDeleted, MessageToDeleteNotFound):
             _message = cls.get(user_id=user_id)
             if _message is not None:
+                if user_id in cls._messages:
+                    cls._messages[user_id].discard(_message.message_id)
                 await bot.delete_message(chat_id=user_id, message_id=_message.message_id)
