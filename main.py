e2c4235550f20876d7dd228df0dea6d53fe06f27
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-03-08 16:15:39 +0300

    Print message and query handlers number.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import loguru
 
-from handlers import dp
+from handlers import dp as dispatcher
 from loader import bot_engine
 
 if __name__ == '__main__':
-    loguru.logger.info(f"Number of handlers: {len(dp.message_handlers.handlers)}.")
+    loguru.logger.info(f"Number of message handlers: {len(dispatcher.message_handlers.handlers)}.")
+    loguru.logger.info(f"Number of callback query handlers: {len(dispatcher.callback_query_handlers.handlers)}.")
     bot_engine.start()
