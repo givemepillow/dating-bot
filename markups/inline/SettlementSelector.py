208c587928a54b1863d2e223b0990f11ce87ff78
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-03-08 16:13:32 +0300

    SettlementSelector update.

--- markups/inline/SettlementSelector.py
+++ markups/inline/SettlementSelector.py
@@ -1,36 +1,48 @@
+from dataclasses import dataclass
+
+import loguru
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.callback_data import CallbackData
 
-from model.Settlement import Settlement
+from model import Settlement
 
-__all__ = ['SettlementSelector']
+
+@dataclass(frozen=True)
+class _Actions:
+    prev = 'prev'
+    next = 'next'
+    back = 'back'
+    select = 'select'
 
 
 class _SettlementSelector:
-    def __init__(self, settlements: [Settlement], count: int):
+    def __init__(self, settlements: [Settlement], count: int, callback_data: CallbackData):
         self.__count: int = count
         self.__end: int = count
         self.__start: int = 0
         self.__settlements: [Settlement] = settlements
+        self._data = callback_data
 
-    def build_markup(self, action=None):
+    def build_markup(self, callback_data):
         markup = InlineKeyboardMarkup(row_width=2)
-        match action:
-            case 'next':
-                self.__next()
-            case 'prev':
-                self.__prev()
+        if callback_data is not None:
+            match callback_data['action']:
+                case _Actions.next:
+                    self._next()
+                case _Actions.prev:
+                    self._prev()
         for s in self.__settlements[self.__start:self.__end]:
             _text = s.name if s.name == s.region else s.name + ', ' + s.region
-            markup.add(InlineKeyboardButton(text=_text, callback_data=s.id))
+            markup.add(InlineKeyboardButton(text=_text, callback_data=self._data.new(_Actions.select, s.id)))
         if len(self.__settlements) > self.__count:
             markup.add(
-                InlineKeyboardButton(text='⬅️', callback_data='prev'),
-                InlineKeyboardButton(text='➡️', callback_data='next')
+                InlineKeyboardButton(text='⬅️', callback_data=self._data.new(_Actions.prev, 0)),
+                InlineKeyboardButton(text='➡️', callback_data=self._data.new(_Actions.next, 0))
             )
-        markup.add(InlineKeyboardButton(text='Назад ↩️', callback_data='back'))
+        markup.add(InlineKeyboardButton(text='Назад ↩️', callback_data=self._data.new(_Actions.back, 0)))
         return markup
 
-    def __next(self):
+    def _next(self):
         if self.__start >= len(self.__settlements) - self.__count:
             self.__end = self.__count
             self.__start = 0
@@ -38,7 +50,7 @@ class _SettlementSelector:
             self.__start += self.__count
             self.__end += self.__count
 
-    def __prev(self):
+    def _prev(self):
         if self.__start == 0:
             _start = (len(self.__settlements) // self.__count) * self.__count
             self.__start = _start if _start != len(self.__settlements) else _start - self.__count
@@ -49,16 +61,28 @@ class _SettlementSelector:
 
 
 class SettlementSelector:
-    storage: {str: _SettlementSelector} = dict()
+    _storage: {str: _SettlementSelector} = dict()
+    data = CallbackData('settlements', 'action', 'settlement_id')
+    actions = _Actions()
 
     @classmethod
-    def init(cls, settlements: [Settlement], user_id: int, count: int = 5):
-        cls.storage[user_id] = _SettlementSelector(settlements=settlements, count=count)
+    def setup(cls, settlements: [Settlement], user_id: int, count: int = 5):
+        cls._storage[user_id] = _SettlementSelector(
+            settlements=settlements,
+            count=count,
+            callback_data=cls.data
+        )
 
     @classmethod
     def clear(cls, user_id):
-        del cls.storage[user_id]
+        try:
+            del cls._storage[user_id]
+        except KeyError as err:
+            loguru.logger.warning(str(err))
 
     @classmethod
-    def update_markup(cls, user_id, action=None):
-        return cls.storage[user_id].build_markup(action)
+    def markup(cls, user_id, callback_data=None):
+        return cls._storage[user_id].build_markup(callback_data)
+
+
+__all__ = ['SettlementSelector']
