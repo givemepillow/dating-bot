52cda039e05317dba75b271291d3f2c9d1576b89
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-03-09 14:45:27 +0300

    Refactoring.

--- loader.py
+++ loader.py
@@ -1,10 +1,18 @@
+import locale
+
+from sqlalchemy.orm import sessionmaker
+
 from utils import WebhookModel, PollingModel
+from orm import database_engine
+from repository import SqlAlchemyRepository
+
 import argparse
 
-__all__ = ['dp', 'bot_engine', 'bot', 'memory_storage']
+__all__ = ['dp', 'bot_engine', 'bot', 'session', 'repository']
+
+locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
 
 parser = argparse.ArgumentParser("main.py")
-
 parser.add_argument("--mode", "-m", nargs=1,
                     help="Run the application in polling mode or webhook mode.",
                     default='polling',
@@ -13,8 +21,13 @@ parser.add_argument("--mode", "-m", nargs=1,
 
 args = parser.parse_args()
 
-bot_engine = WebhookModel() if args.mode[0] == 'webhook' else PollingModel()
+db_engine = database_engine(drop=False, echo=False)
+Session = sessionmaker(bind=db_engine)
+session = Session()
 
+repository = SqlAlchemyRepository(session=session)
+
+bot_engine = WebhookModel() if args.mode[0] == 'webhook' else PollingModel()
 memory_storage = bot_engine.get_storage()
 dp = bot_engine.get_dispatcher()
 bot = bot_engine.get_bot()
