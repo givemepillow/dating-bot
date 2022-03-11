ca6d4399cf4794e020098edeb6428c8fa2b8212a
Tema <im-unstoppable@mail.ru>
2022-03-11 03:22:04 +0300

    Returns age sufix

--- toolkit/age.py
+++ toolkit/age.py
@@ -5,3 +5,11 @@ def age(date_of_birth):
     if date_of_birth.month <= today.month and date_of_birth.day <= today.day:
        return today.year - date_of_birth.year
     return today.year - date_of_birth.year - 1
+
+def sufix(age):
+    ending = age % 10
+    if ending == 1:
+        return 'год'
+    if ending > 4 or 4 < age < 21 or ending == 0:
+        return 'лет'
+    return 'года'
