f1036540770b1eeaeee685dbb80fea40431fd71e
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-03-11 04:10:00 +0300

    New states.

--- states/states.py
+++ states/states.py
@@ -4,8 +4,10 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 class QState(StatesGroup):
     start = State()
     input_name = State()
+    select_gender = State()
+    select_looking_for = State()
     search_settlement = State()
     select_settlement = State()
     select_date = State()
-    select_gender = State()
-    select_looking_for = State()
+    get_photo = State()
+    finish = State()
