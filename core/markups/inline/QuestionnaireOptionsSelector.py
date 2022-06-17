f1ef0a5b9745ec7cde67d68aab75bbb9a4710327
caVenikk <sqqvt1@gmail.com>
2022-06-17 13:07:44 +0300

    Removed ability to return to previous questionnaire two or more times; buttons now will take up the entire row

--- core/markups/inline/QuestionnaireOptionsSelector.py
+++ core/markups/inline/QuestionnaireOptionsSelector.py
@@ -5,13 +5,20 @@ from aiogram.utils.emoji import emojize
 class QuestionnaireOptionsSelector:
     @staticmethod
     def markup(action=None):
-        _markup = InlineKeyboardMarkup(row_width=2)
+        _markup = InlineKeyboardMarkup(row_width=1)
         if action == 'report':
             _markup.add(
                 InlineKeyboardButton(text=emojize('К предыдущей :arrow_left:'),
                                      callback_data='previous'),
                 InlineKeyboardButton(text=emojize('Пожаловаться :warning:'),
                                      callback_data='_')
+            )
+        elif action == 'previous':
+            _markup.add(
+                InlineKeyboardButton(text=' ',
+                                     callback_data='_'),
+                InlineKeyboardButton(text=emojize('Пожаловаться :warning:'),
+                                     callback_data='report')
             )
         else:
             _markup.add(
