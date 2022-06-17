397001c5cbbfadaa23cba6a7ac173e3c2839b0b6
caVenikk <sqqvt1@gmail.com>
2022-06-17 15:21:00 +0300

    New handlers
    New handler file that contains handlers for enabling/disabling person's questionnaire

--- core/handlers/__init__.py
+++ core/handlers/__init__.py
@@ -2,5 +2,6 @@ from .backs import dp
 from .qest import dp
 from .plug import dp
 from .feed import dp
+from .interruptor import dp
 
 __all__ = ['dp']
---
+++ core/handlers/interruptor.py
@@ -0,0 +1,26 @@
+from aiogram.types import Message
+
+from core import Feed
+from core.markups.text import *
+from core.states import FState, QState
+from loader import dp, repository
+from .templates import send_quest_if_person
+
+
+@dp.message_handler(text='Я больше не хочу никого искать', state=FState.sleeping)
+async def disable(message: Message):
+    await message.answer(text='Хорошо, сообщи, когда захочешь вернуться!',
+                         reply_markup=disabled_keyboard
+                         )
+    repository.disable_person(message.from_user.id)
+    QState.zero.set()
+
+
+@dp.message_handler(text='Вернуться к поиску', state=QState.zero)
+async def enable(message: Message):
+    _user_id = message.from_user.id
+    await message.answer(text='Приступаем к поиску анкет!', reply_markup=rate_keyboard)
+    repository.enable_person(_user_id)
+    FState.looking.set()
+    person = Feed(_user_id).next
+    await send_quest_if_person(_user_id, person)
